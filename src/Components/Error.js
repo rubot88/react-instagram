5765881398e503851a28a19fc08fd63197907abb
Rustam Botirov <rustambotirov88@gmail.com>
2019-10-27 11:46:36 +0200

    add error component

---
+++ src/Components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import error from '../error.png';
+
+const ErrorMessage = () => {
+    return (
+            <img src={error} alt="Error" />
+    )
+}
+
+export default ErrorMessage;
