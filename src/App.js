25a7b416dd195900abacc5f5ea6b62d07a175906
Rustam Botirov <rustambotirov88@gmail.com>
2019-10-27 10:36:24 +0200

    instaService is added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Components/Header';
-import Feed from './Components/Feed';
+import Header from './components/Header';
+import Feed from './components/Feed';
 export default function App() {
   return (
     <div className='App'>
---
+++ src/services/instaService.js
@@ -0,0 +1,19 @@
+export default class instaService {
+    constructor() {
+        this._apiBase = 'http://localhost:3000';
+    }
+
+    getResource = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch ${url}; received ${res.status}`);
+        }
+        return await res.json();
+    }
+
+    gerAllPosts = async () => {
+        const res = await this.getResource('/posts/');
+        return res;
+    }
+}
